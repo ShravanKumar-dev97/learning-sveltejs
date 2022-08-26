9afd352217042547477cd134c51dafb1644ebd15
Shravan Kumar <shravankumar@Shravans-Mac-mini.local>
2022-08-27 01:19:51 +0530

    wip1 - adding name card

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,79 @@
 <script>
-	export let name;
+	import ContactCard from './ContactCard.svelte'
+
+	let name = "";
+	let age = 25;
+	let title = "";
+	let description = "";
+	
+
+	$: newName = name.toUpperCase();
+
+
+
+
+
+	function incrementAge (){
+		age += 1;
+	}
+
+	function decrementAge (){
+		if(age>0){
+		age -= 1;
+		}
+	}
+
+	function changeName(){
+		name = "Rocky"
+		age = 33
+	}
+
+
+	// // for unidirectional method, we have to go with event
+	// function nameInput(event) {
+	// 	const enteredValue = event.target.value;
+	// 	name = enteredValue;
+	// }
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {newName}!</h1>
+
+	<!-- by default the data flow in svelte is unidirectional -->
+	<!-- <input type="text" value="{name}" on:input="{nameInput}" placeholder="Enter your Name"> -->
+
+	<!-- <button on:click="{changeName}">Change Name</button> -->
+	<!-- <button on:click="{decrementAge}">Back to Past</button> -->
+	<!-- <button on:click="{incrementAge}">Foward to Future</button> -->
+
+	<!-- Two-way Binding  shortcut-->
+	<input type="text" bind:value="{name}" placeholder="Enter your Name"> <br>
+	<input type="text" bind:value="{title}" placeholder="Job Title"> <br>
+	<input type="text" bind:value="{description}" placeholder="Description">
+
+	<ContactCard 
+		userName="{name}" 
+		jobTitle="{title}"
+		description="{description}"
+	/>
+	
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		/* max-width: 240px; */
 		margin: 0 auto;
 	}
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		/* text-transform: uppercase; */
+		font-size: 2em;
+		font-weight: 200;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
---
+++ src/ContactCard.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let userName;
+    export let jobTitle;
+    export let description;
+</script>
+
+<div>
+    <header>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <h1>{userName}</h1>
+            <h2>{jobTitle}</h2>
+        </div>
+    </header>
+
+    <div>
+        <p>{description}</p>
+    </div>
+</div>
+
+<style>
+
+</style>
